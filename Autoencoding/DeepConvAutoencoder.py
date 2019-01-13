f2429eec2cc159b788d3624641660a503e4b5d87
Kiran Bhattacharyya <bhattacharyyakiran12@gmail.com>
2019-01-13 14:07:24 -0600

    add comments

--- Autoencoding/DeepConvAutoencoder.py
+++ Autoencoding/DeepConvAutoencoder.py
@@ -1,3 +1,12 @@
+## Name: Deep convolutional autoencoder
+## Description: Reads in the 32 by 32 image matrix
+##              for each image and then trains a deep convolutional autoencoder
+##              on the entire sketchy dataset using keras and tensorflow.
+##              Then checks to see how well it can recreate images it has
+##              never seen with some visualization
+##              NOTE: This code uses the output from save32by32ForAll.py
+## Author: Kiran Bhattacharyya
+
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,39 +16,40 @@ from keras.models import Model
 from keras import backend as K
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## Build deep autoencoder
+## Build deep convolutional autoencoder
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 encoded_dim = 128
 
-input_img = Input(shape=(32, 32, 1))
+input_img = Input(shape=(32, 32, 1)) # define so rgb images can also be processed if necessary
 
-x = Conv2D(32, 3, activation='relu', padding='same')(input_img)
+x = Conv2D(32, 3, activation='relu', padding='same')(input_img) # convolutional layers
 x = MaxPooling2D(padding='same')(x)
 x = Conv2D(16, 3, activation='relu', padding='same')(x)
 x = MaxPooling2D(padding='same')(x)
 x = Conv2D(8, 3, activation='relu', padding='same')(x)
-x = Flatten()(x)
+x = Flatten()(x) # flatten convolutional layers and send to densely connected layers 
 x = Dense(256, activation='relu')(x)
 encoded = Dense(encoded_dim, activation='relu')(x)
 
 # create encoder network
 encoder = Model(input_img, encoded)
 
-y = Dense(256, activation='relu')(encoded)
+# define decoder model 
+y = Dense(256, activation='relu')(encoded) # reflect flat, densely connected layers 
 y = Dense(512, activation='relu')(y)
 y = Reshape((8,8,8))(y)
-y = Conv2D(16, 3, activation='relu', padding='same')(y)
+y = Conv2D(16, 3, activation='relu', padding='same')(y) # reflect convolutional layers 
 y = UpSampling2D()(y)
 y = Conv2D(32, 1, activation='relu')(y)
 y = UpSampling2D()(y)
-decoded = Conv2D(1, 3, activation='sigmoid', padding='same')(y)
+decoded = Conv2D(1, 3, activation='sigmoid', padding='same')(y) # output is 32 by 32 by 1 
 
 # create autoencoder network
 autoencoder = Model(input_img, decoded)
 
 # create decoder network
-# create separate decoder model
+# create separate decoder model by taking the last layers of the autoencoder network 
 encoded_input = Input(shape=(encoded_dim,))
 decoded_layer1 = autoencoder.layers[-8]
 decoded_layer2 = autoencoder.layers[-7]
@@ -106,7 +116,7 @@ encoded_imgs = encoder.predict(array[nTrain:,:,:,:])
 decoded_imgs = decoder.predict(encoded_imgs)
 
 n = 10  # how many digits we will display
-startIndx = 4521
+startIndx = 4521 # start at some random point in the validation set 
 plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
